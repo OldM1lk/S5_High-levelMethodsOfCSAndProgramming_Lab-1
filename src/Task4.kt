9882c5e5084a650c942990a681ee3a0f0af16e2e
OldM1lk <mrlaimwin@gmail.com>
2025-09-17 10:54:14 +0700

    #1 №3 Remove explicit data type specify

--- src/Task4.kt
+++ src/Task4.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     args.sort()
-    val distinctArgsCount: Map<String, Int> = args.groupingBy { it }.eachCount()
+    val distinctArgsCount = args.groupingBy { it }.eachCount()
     distinctArgsCount.forEach { println(it.key + " " + it.value) }
 }
