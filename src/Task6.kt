bf00a6bbff59908abf507e4f2bbe125c63299655
OldM1lk <mrlaimwin@gmail.com>
2025-09-17 19:24:14 +0700

    #1 №5,6 Separate functionality, try to fix input

--- src/Task6.kt
+++ src/Task6.kt
@@ -1,14 +1,24 @@
 fun main(args: Array<String>) {
-    val distinctArgsCount: Map<String, Int> = if (args.isNotEmpty()) {
-        args.groupingBy { it }.eachCount()
-    } else {
-        val input = readlnOrNull()
-        input?.split(" ")?.groupingBy { it }?.eachCount() ?: emptyMap()
-    }
-    val sortedDistinctArgsCount = distinctArgsCount
-        .toList()
-        .sortedWith(compareByDescending<Pair<String, Int>> { it.second }
-            .thenBy { it.first })
-        .toMap()
-    sortedDistinctArgsCount.forEach { println(it.key + " " + it.value) }
+    val words = getWords(args)
+    val stats = processWords(words)
+    printStats(stats)
 }
+
+fun getWords(args: Array<String>): List<String> =
+    if (args.isNotEmpty()) {
+        args.toList()
+    } else {
+        val input = readlnOrNull().orEmpty()
+        input.split("\\s+".toRegex()).filter { it.isNotBlank() }
+    }
+
+fun processWords(words: List<String>): List<Pair<String, Int>> =
+    words
+        .sorted()
+        .groupingBy { it }
+        .eachCount()
+        .toList()
+        .sortedByDescending { it.second }
+
+fun printStats(stats: List<Pair<String, Int>>) =
+    stats.forEach { (word, count) -> println("$word $count") }
