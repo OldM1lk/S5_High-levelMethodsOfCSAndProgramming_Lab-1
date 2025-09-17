9ba6a8d86abf1697c42f55443ee1db36a4e2217c
OldM1lk <mrlaimwin@gmail.com>
2025-09-17 10:50:57 +0700

    #1 №2 distinct really works

--- src/Task3.kt
+++ src/Task3.kt
@@ -1,13 +1,6 @@
 fun main(args: Array<String>) {
-    // distinct() не работает пришлось делать костыли
-    val distinctArgs = mutableListOf<String>()
-
-    args.forEach {
-        if (!distinctArgs.contains(it)) {
-            distinctArgs.add(it)
-        }
-    }
-
-    distinctArgs.sort()
-    distinctArgs.forEach { println(it) }
+    args
+        .distinct()
+        .sorted()
+        .forEach(::println)
 }
